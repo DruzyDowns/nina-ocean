395da52d05659401f1edcc29a10cf60e9e73a614
Andrew Downs <druzydowns@gmail.com>
2021-08-22 15:59:47 -0400

    Install Axios, reorganize App.js and index.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  componentDidMount() {
+    console.log("album data");
+  }
+
+  render() {
+    return (
+      <div className="App relative">
+        <div className="w-full flex justify-center">
+          <h1 className="uppercase font-bold text-5xl">ocean</h1>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
